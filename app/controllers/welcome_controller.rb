5b611d52d245ea830b15e7c0cd26effe08846bb9
buaamm <buaamm@gmail.com>
2015-06-11 14:23:52 +0800

    upload video

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def upload
+    uploaded_io = params[:video]
+    File.open(Rails.root.join('public', 'media', uploaded_io.original_filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+  end
+  
 end
